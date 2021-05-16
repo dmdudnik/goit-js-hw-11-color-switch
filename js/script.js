74ce6f7c1dac17c51a6abcbee1f086ca439589b3
Dmitriy Dudnik <ddudnik79@gmail.com>
2021-05-16 23:52:25 +0300

    исправлены пути к идентификаторам

--- js/script.js
+++ js/script.js
@@ -7,6 +7,44 @@ const colors = [
   '#795548',
 ];
 
-// const randomIntegerFromInterval = (min, max) => {
-//     return Math.floor(Math.random() * (max - min + 1) + min);
-// };
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+};
+
+let isActive = false;
+let intervalId = 0;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function randomIndex() {
+  return randomIntegerFromInterval(0, colors.length - 1);
+}
+
+function randomColor() {
+  for (let i = 0; i < colors.length; i += 1) {
+    if (colors.indexOf(colors[i]) === randomIndex()) {
+      return colors[i];
+    }
+  }
+}
+
+function changeBackground() {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  intervalId = setInterval(() => {
+    document.querySelector('body').style.background = randomColor();
+  }, 1000);
+}
+
+function stopChange() {
+  isActive = false;
+  clearInterval(intervalId);
+}
+
+refs.startBtn.addEventListener('click', changeBackground);
+refs.stopBtn.addEventListener('click', stopChange);
